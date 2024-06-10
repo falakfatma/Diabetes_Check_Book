6b27515344291b60081191de08ba8a648539b70b
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2024-06-10 14:12:32 +0000

    table title added

--- src/components/tanstackTable.jsx
+++ src/components/tanstackTable.jsx
@@ -1,66 +1,66 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Box, Heading } from "@chakra-ui/react";
-import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
-import {DATA} from '../data';
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { DATA } from "../data";
 
-const columns =[
+const columns = [
   {
-    accessorKey:'first_Name',
-    header :'First Name',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "firstName",
+    header: "First Name",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey:'last_Name',
-    header :'Last Name',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "lastName",
+    header: "Last Name",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey:'age',
-    header :'Age',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "age",
+    header: "Age",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey:'visits',
-    header :'Visits',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "visits",
+    header: "Visits",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey:'progress',
-    header :'Progress',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "progress",
+    header: "Progress",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey:'status',
-    header :'Status',
-    cell: (props) => {
-      return <p>{props.getValue()}</p>
-    }
+    accessorKey: "status",
+    header: "Status",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
-  
-]
+];
 
-const tanstackTable = () => {
-  const [data , setData] = useState(DATA)
-
+function tanstackTable() {
+  const [data, setData] = useState(DATA);
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel:getCoreRowModel()
-  })
-  console.log(table.getHeaderGroups())
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <Box className="table">
+       Grand Parent Box
+      {table.getHeaderGroups().map((headerGroup) => {
+        return( 
+        <Box key={headerGroup.id} className="th">
+          {headerGroup.headers.map((headers) => {
+            return (
+              <Box className="tr" key={headers.id}>
+                {headers.column.columnDef.header}
+               </Box>
+             );
+          })}
+        </Box>
+        )
+      })}
     </Box>
   );
-};
+}
 export default tanstackTable;
