bee20a8121df4c03872b84dbe1025b143e6349cd
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2024-06-10 17:27:32 +0000

    Tags For Table and UI is Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Heading, Box, Text } from "@chakra-ui/react";
+import { Heading, Box, Text, TableContainer, Table } from "@chakra-ui/react";
 import "./App.css";
-import TanStackTable from "./components/tanStackTable"
+import TanStackTable from "./components/tanStackTable";
 
 export default function App() {
   return (
     <main>
       <Heading> TanStackTable </Heading>
-      <Box className="table">
         <TanStackTable />
-      </Box>
     </main>
   );
 }
--- src/components/tanStackTable.jsx
+++ src/components/tanStackTable.jsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { Box, Heading, Text, Table,
+         Thead,
+         Tbody,
+         Tfoot,
+         Tr,
+         Th,
+         Td,
+         TableCaption,
+         TableContainer, } from "@chakra-ui/react";
+import {
+  getCoreRowModel,
+  useReactTable,
+  flexRender,
+} from "@tanstack/react-table";
 import { DATA } from "../data";
 
 const columns = [
@@ -43,28 +55,77 @@ function TanStackTable() {
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
-  console.log(table.getRowModel()) ;
+  // console.log(table.getRowModel());
   return (
-    <>
-      {table.getHeaderGroups().map((headerGroup) => {
-        return( 
-        <Box key={headerGroup.id} className="th">
-          {headerGroup.headers.map((headers) => {
-            return (
-              <Box className="headers" key={headers.id}>
-               <Heading fontSize='xl'>
-                 {headers.column.columnDef.header}
-               </Heading> 
-               </Box>
-             );
-          })}
-        </Box>
-        )
-      })}
-      {table.getRowModel().rows.map((row) =>
-        {row}
-      )}
-    </>
+    <TableContainer>
+      <Table variant='simple'>
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        {table.getHeaderGroups().map((headerGroup) => {
+          return (
+            <Tr key={headerGroup.id}>
+              {headerGroup.headers.map((headers) => {
+                return (
+                    <Th fontSize="xl" key={headers.id}>
+                      {headers.column.columnDef.header}
+                    </Th>
+                );
+              })}
+            </Tr>
+          );
+        })}
+  
+        {table.getRowModel().rows.map((row) => {
+          console.log(row.getVisibleCells());
+          return (
+            <Tr key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                return (
+                    <Td key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </Td>
+                );
+              })}
+            </Tr>
+          );
+        })}
+      </Table>
+    </TableContainer>
+
+
+    // <TableContainer>
+    //   <Table variant='simple'>
+    //     <TableCaption>Imperial to metric conversion factors</TableCaption>
+    //     <Thead>
+    //       <Tr>
+    //         <Th>To convert</Th>
+    //       </Tr>
+    //     </Thead>
+    //     <Tbody>
+    //       <Tr>
+    //         <Td>inches</Td>
+    //         <Td>millimetres (mm)</Td>
+    //         <Td isNumeric>25.4</Td>
+    //       </Tr>
+    //       <Tr>
+    //         <Td>feet</Td>
+    //         <Td>centimetres (cm)</Td>
+    //         <Td isNumeric>30.48</Td>
+    //       </Tr>
+    //       <Tr>
+    //         <Td>yards</Td>
+    //         <Td>metres (m)</Td>
+    //         <Td isNumeric>0.91444</Td>
+    //       </Tr>
+    //     </Tbody>
+    //     <Tfoot>
+    //       <Tr>
+    //         <Th>To convert</Th>
+    //         <Th>into</Th>
+    //         <Th isNumeric>multiply by</Th>
+    //       </Tr>
+    //     </Tfoot>
+    //   </Table>
+    // </TableContainer>
   );
 }
 export default TanStackTable;
